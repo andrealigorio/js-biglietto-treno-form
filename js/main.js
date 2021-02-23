0a96b952cfde9a1746d4cc9493373ec636e6c04a
Andrea Ligorio <andrea85kro@gmail.com>
2021-02-23 11:29:50 +0100

    Applicate ultime correzioni

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ const price = 0.21;
 const youngDiscount = 0.80;
 const oldDiscount = 0.60;
 
-var trainWagon = Math.floor(Math.random()*10) + 1;
-var trainCode = Math.floor(Math.random()*10000) + 90000;
 
 
 //DEFINISCO AZIONI DEL BOTTONE GENERA
 var generate = document.getElementById("generate");
 generate.addEventListener("click", 
-    function() {
+function() {
+    var trainWagon = Math.floor(Math.random()*10) + 1;
+    var trainCode = Math.floor(Math.random()*10000) + 90000;
     var show = document.getElementById("box_ticket");
     show.className = "show"
     var nameSurname = document.getElementById("name_surname").value;
@@ -37,10 +37,13 @@ generate.addEventListener("click",
 var cancel = document.getElementById("cancel");
 cancel.addEventListener("click",
     function () {
+        document.getElementById("box_ticket").className = "hidden";
+        document.getElementById("discount").innerHTML = "";
+        document.getElementById("name").innerHTML = "";
+        document.getElementById("train_wagon").innerHTML = "";
+        document.getElementById("train_code").innerHTML = "";
         document.getElementById("name_surname").value = "";
         document.getElementById("kilometres").value = "";
         document.getElementById("ages").value = "under18";
-        var hidden = document.getElementById("box_ticket");
-        hidden.className = "hidden";
     }
 );
